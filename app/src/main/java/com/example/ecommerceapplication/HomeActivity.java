7702e21330c91b37f85b59615de25f2866c2fc4d
Nikolay Pleshkov <pleshkovn1kk@gmail.com>
2022-10-23 18:58:56 +0300

    feat: sample home layout

--- app/src/main/java/com/example/ecommerceapplication/HomeActivity.java
+++ app/src/main/java/com/example/ecommerceapplication/HomeActivity.java
@@ -12,24 +12,21 @@ import com.google.firebase.auth.FirebaseUser;
 public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
 
-    Button logoutBtn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        logoutBtn = (Button) findViewById(R.id.logoutBtn);
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        logoutBtn.setOnClickListener(view -> {
-            firebaseAuth.signOut();
-            Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(mainActivity);
-
-            finish();
-        });
+//        logoutBtn.setOnClickListener(view -> {
+//            firebaseAuth.signOut();
+//            Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+//            startActivity(mainActivity);
+//
+//            finish();
+//        });
     }
 
     @Override
