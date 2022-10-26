09bc35919940a7a1a1558da71b650d151703cf4d
Nikolay Pleshkov <pleshkovn1kk@gmail.com>
2022-10-26 23:45:23 +0300

    Update HomeActivity.java

--- app/src/main/java/com/example/ecommerceapplication/HomeActivity.java
+++ app/src/main/java/com/example/ecommerceapplication/HomeActivity.java
@@ -1,18 +1,26 @@
 package com.example.ecommerceapplication;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuView;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.ecommerceapplication.Entity.Category;
 import com.example.ecommerceapplication.Holder.MenuHolder;
+import com.example.ecommerceapplication.Listener.ItemClickListener;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
@@ -54,6 +62,51 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             Intent cartActivity = new Intent(HomeActivity.this, CartActivity.class);
             startActivity(cartActivity);
         });
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggleDrawer = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(toggleDrawer);
+        toggleDrawer.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View navbarView = navigationView.getHeaderView(0);
+        userDisplayName = navbarView.findViewById(R.id.userDisplayName);
+
+        recyclerView = findViewById(R.id.recycler_menu);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new FirebaseRecyclerAdapter<Category, MenuHolder>(Category.class, R.layout.menu_item, MenuView.class, categoryReference) {
+
+            @NonNull
+            @Override
+            public MenuHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull MenuHolder holder, @SuppressLint("RecyclerView") int position, @NonNull Category model) {
+                holder.textView.setText(model.getName());
+
+                final Category clickitem = model;
+
+                holder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int pos, boolean isLong) {
+                        Intent itemList = new Intent(HomeActivity.this, ItemList.class);
+                        itemList.putExtra("CategoryId", adapter.getRef(position).getKey());
+
+                        startActivity(itemList);
+                    }
+                });
+            }
+        };
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
