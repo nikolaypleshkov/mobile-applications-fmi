755ec4a85489b244068c0d4f5f4f9f97bcbecc58
Nikolay Pleshkov <pleshkovn1kk@gmail.com>
2022-11-01 18:27:40 +0200

    feat: temporary put items in firebase database

--- app/src/main/java/com/example/ecommerceapplication/MainActivity.java
+++ app/src/main/java/com/example/ecommerceapplication/MainActivity.java
@@ -5,16 +5,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.ecommerceapplication.Entity.Item;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     Button signUpBtn, signInBtn;
     TextView textSlogan;
+
+    FirebaseDatabase database;
+    DatabaseReference itemList;
 
     private FirebaseAuth firebaseAuth;
     @Override
@@ -39,5 +46,17 @@ public class MainActivity extends AppCompatActivity {
             Intent signUp = new Intent(MainActivity.this, SignUpActivity.class);
             startActivity(signUp);
         });
+
+        Item[] items = new Item[]{
+                new Item("Item Name", "", "Item Description", "300", "1"),
+                new Item("Item 2 Name", "", "Item 2 Description", "500", "2")
+        };
+
+        database = FirebaseDatabase.getInstance("https://ecommerce-30ed3-default-rtdb.europe-west1.firebasedatabase.app/");
+
+        for (Item item : items){
+            database.getReference("Items").child(item.getMenuId()).setValue(item);
+            Toast.makeText(MainActivity.this, "Item added", Toast.LENGTH_LONG).show();
+        }
     }
 }
