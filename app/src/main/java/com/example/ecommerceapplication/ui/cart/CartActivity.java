4ff979ef4f9f1293b61414ce860607e7eee0c0f6
Nikolay Pleshkov <pleshkovn1kk@gmail.com>
2022-11-23 18:12:44 +0200

    feat: cart activity

---
+++ app/src/main/java/com/example/ecommerceapplication/ui/cart/CartActivity.java
@@ -0,0 +1,70 @@
+package com.example.ecommerceapplication.ui.cart;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.ecommerceapplication.R;
+import com.example.ecommerceapplication.data.model.Order;
+import com.example.ecommerceapplication.database.DBActivity;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class CartActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    FirebaseFirestore db;
+
+    TextView txtTotalPrice;
+    Button btnPlaceOrder;
+
+    List<Order> cart = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        db = FirebaseFirestore.getInstance();
+
+        recyclerView = findViewById(R.id.listCart);
+        recyclerView.setLayoutManager(layoutManager);
+
+        txtTotalPrice = findViewById(R.id.total);
+        btnPlaceOrder = findViewById(R.id.btnPlaceOrder);
+
+        btnPlaceOrder.setOnClickListener(view -> {
+            //TODO: show alert dialog
+        });
+
+        //TODO: load cart items
+    }
+
+    private void showAlertDialog(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(CartActivity.this);
+    }
+
+    private void loadCartItems(){
+        cart = new DBActivity(this).getCartItems();
+        recyclerView.setAdapter(adapter);
+
+        int total = 0;
+
+        for(Order order : cart){
+            total += (Integer.parseInt(order.getPrice())) * (Integer.parseInt(order.getQuantity()));
+            NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.GERMAN);
+            txtTotalPrice.setText(numberFormat.format(total));
+        }
+    }
+}
